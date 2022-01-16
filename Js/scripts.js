7a52b85731e26e49604efe9e6e37d73668c07149
michael-muga <mugamichaelshawn@gmail.com>
2022-01-16 20:21:12 +0300

    function to alert when delivery option is checked added

---
+++ Js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    //event.preventDefault();
+    $('#delivery').change(function(event){
+        if (this.checked){
+            alert("you can pick up your order in the next 1hour")
+        }
+        else{
+            alert("The delivery service will cost you extra ksh.100 on your total price")
+        }
+
+    })
+})
